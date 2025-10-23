8c0fc73ba9e05f5757d349033429aabfb470bff6
riorichard98 <riorichard12@gmail.com>
2025-10-23 18:06:38 +0700

    fix: no bulk object for register(memory leak)

--- domains/user/user.usecase.ts
+++ domains/user/user.usecase.ts
@@ -2,6 +2,5 @@ import UserEntity from "./user.entity";
 
 export interface UserUsecase {
     bulkRegister(emails: string[], roles: string[], passwords: string[]): Promise<void>;
-
     login(email: string, password: string): Promise<UserEntity>;
 }
