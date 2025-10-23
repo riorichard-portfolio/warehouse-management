1030fee74831d6ff7ce85820856bb07949f404b1
riorichard98 <riorichard12@gmail.com>
2025-10-23 22:02:00 +0700

    feat: validate entire body instead

--- domains/user/user.usecase.ts
+++ domains/user/user.usecase.ts
@@ -2,5 +2,5 @@ import UserEntity from "./user.entity";
 
 export default interface UserUsecase {
     login(email: string, password: string): Promise<UserEntity>;
-    validateRegistration(email: string): Promise<string>; // return token for next registration
+    validateRegistration(email: string, password: string, role: string): Promise<string>; // return token for next registration
 }
