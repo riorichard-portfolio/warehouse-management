58bc9a2d4302aff45d635e260e44386a95ca1c5e
riorichard98 <riorichard12@gmail.com>
2025-11-07 18:33:29 +0700

    feat: primitive string wrapper done

---
+++ src/common/string.wrapper.ts
@@ -0,0 +1,37 @@
+import Constant from '@/common/constants'
+
+const errorNullNotVerified = "isNull() must be called before value()"
+const errorNotProperNullVerifyUse = "data is null :isValidValue() must be used properly to avoid null"
+
+export default class Characters implements Constant.NotNullCharacters, Constant.NullableCharacters {
+    private readonly stringData: string | null = null
+    private readonly isStringDataNull: boolean = true
+    private nullValueNotVerified: boolean = true
+    constructor(unknownData: unknown) {
+        try {
+            if (typeof unknownData === 'string') {
+                this.stringData = unknownData
+                this.isStringDataNull = false
+            }
+        } catch (_) { }
+    }
+    public value(): string {
+        if (this.nullValueNotVerified) throw new Error(errorNullNotVerified)
+        if (typeof this.stringData === 'string') {
+            return this.stringData
+        } else {
+            // if this triggered means value === null 
+            // means accessing null value not in condition isNotNull after verifying
+            // or still using it if it already verified null
+            throw new Error(errorNotProperNullVerifyUse)
+        }
+    }
+    public isNotNull(): boolean {
+        this.nullValueNotVerified = false
+        return !this.isStringDataNull
+    }
+    public isNull(): boolean {
+        this.nullValueNotVerified = false
+        return this.isStringDataNull
+    }
+}
