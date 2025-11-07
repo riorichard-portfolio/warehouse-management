ec42296169d33976bded6a60564f244e0e1ffa87
riorichard98 <riorichard12@gmail.com>
2025-11-07 18:57:56 +0700

    feat: primitive number wrapper done

---
+++ src/common/number.wrapper.ts
@@ -0,0 +1,45 @@
+import Constant from '@/common/constants'
+
+const errorNullNotVerified = "isNull() must be called before value()"
+const errorNotProperNullVerifyUse = "data is null :isValidValue() must be used properly to avoid null"
+
+export default class Decimal implements Constant.NotNullDecimal, Constant.NullableDecimal {
+    private readonly numberData: number | null = null
+    private readonly isNumberDataNull: boolean = true
+    private nullValueNotVerified: boolean = true
+    constructor(unknownData: unknown) {
+        try {
+            if (typeof unknownData === 'number' && this.isNotNaNNumber(unknownData)) {
+                if (this.isNumberNotInfinity(unknownData)) {
+                    this.numberData = unknownData
+                    this.isNumberDataNull = false
+                }
+            }
+        } catch (_) { }
+    }
+    private isNotNaNNumber(unknownData: unknown): boolean {
+        return !Number.isNaN(unknownData)
+    }
+    private isNumberNotInfinity(unknownNumberData: number) {
+        return isFinite(unknownNumberData)
+    }
+    public value(): number {
+        if (this.nullValueNotVerified) throw new Error(errorNullNotVerified)
+        if (typeof this.numberData === 'number') {
+            return this.numberData
+        } else {
+            // if this triggered means value === null 
+            // means accessing null value not in condition isNotNull after verifying
+            // or still using it if it already verified null
+            throw new Error(errorNotProperNullVerifyUse)
+        }
+    }
+    public isNotNull(): boolean {
+        this.nullValueNotVerified = false
+        return !this.isNumberDataNull
+    }
+    public isNull(): boolean {
+        this.nullValueNotVerified = false
+        return this.isNumberDataNull
+    }
+}
