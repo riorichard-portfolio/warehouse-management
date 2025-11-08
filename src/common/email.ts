f828b95e9696ecb0e71e3d2f317852ec50b2eaa8
riorichard98 <riorichard12@gmail.com>
2025-11-08 22:32:50 +0700

    feat: string wrapper for email data

---
+++ src/common/email.ts
@@ -0,0 +1,66 @@
+import { OptEmail, Email } from "./primitive.abstractions/primitive.unique.abstraction";
+
+const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+const errorNullNotVerified = "isNull()/isNotNull() must be called before value()"
+const errorNotProperNullVerifyUse = "data is null :isNull()/isNotNull() must be used properly to avoid null"
+const errorInvalidTypeForEmail = "value in Email MUST be string"
+const errorStringIsNotAnEmail = "value in NotNullEmail MUST be valid email string"
+
+export class NullableEmail implements OptEmail {
+    private readonly email: string | null = null;
+    private readonly isEmailNull: boolean = true
+    private nullValueNotVerified: boolean = true;
+
+    constructor(unknownData: unknown) {
+        if (typeof unknownData == 'string') {
+            if (this.isEmailValid(unknownData)) {
+                this.email = unknownData
+            }
+        }
+    }
+    private isEmailValid(unknownString: string): boolean {
+        return emailRegex.test(unknownString)
+    }
+
+    public value(): string {
+        if (this.nullValueNotVerified) throw new Error(errorNullNotVerified)
+        if (typeof this.email === 'string') {
+            return this.email
+        } else {
+            // if this triggered means value === null 
+            // means accessing null value not in condition isNotNull after verifying
+            // or still using it if it already verified null
+            throw new Error(errorNotProperNullVerifyUse)
+        }
+    }
+
+    public isNotNull(): boolean {
+        this.nullValueNotVerified = false
+        return !this.isEmailNull
+    }
+    public isNull(): boolean {
+        this.nullValueNotVerified = false
+        return this.isEmailNull
+    }
+}
+
+export class NotNullEmail implements Email {
+    private readonly email: string
+    constructor(stringData: string) {
+        if (typeof stringData === 'string') {
+            if(this.isEmailValid(stringData)){
+                this.email = stringData
+            } else {
+                throw new Error(errorStringIsNotAnEmail)
+            }
+        } else {
+            throw new Error(errorInvalidTypeForEmail)
+        }
+    }
+    private isEmailValid(unknownString: string): boolean {
+        return emailRegex.test(unknownString)
+    }
+    public value(): string {
+        return this.email
+    }
+}
