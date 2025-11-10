5a4672b594df86a0ce58a3e2b7bffaea235cd512
riorichard98 <riorichard12@gmail.com>
2025-11-10 23:54:20 +0700

    feat: add chaining method abstraction

--- src/common/primitive.abstractions/primitive.wrapper.abstraction.ts
+++ src/common/primitive.abstractions/primitive.wrapper.abstraction.ts
@@ -9,6 +9,15 @@ export interface OptStr extends Str { // nullable means also able to Notnullable
 export interface Num {
     value(): number;
     finish(): void;
+    multiplyBy(number: Num): Num
+    dividedBy(number: Num): Num
+    add(number: Num): Num
+    minus(number: Num): Num
+    greaterThan(number: Num): Bool
+    greaterOrEqualThan(number: Num): Bool
+    lessThan(number: Num): Bool
+    lessOrEqualThan(number: Num): Bool
+    equalTo(number: Num): Bool
 }
 export interface OptNum extends Num {
     isNotNull(): boolean;
@@ -19,6 +28,8 @@ export interface Bool {
     no(): boolean // is it no (value === false => output true , else false)
     condition(): boolean // true or false
     finish(): void;
+    // or(condition: Bool): Bool;
+    // and(condition: Bool): Bool;
 }
 export interface OptBool extends Bool {
     isNotNull(): boolean;
