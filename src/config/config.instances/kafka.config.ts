88b5e9f4da592b7af8d14206a5dccd3fa437a48a
riorichard98 <riorichard12@gmail.com>
2025-11-13 16:15:56 +0700

    feat: kafka config done with getAllVars

--- src/config/config.instances/kafka.config.ts
+++ src/config/config.instances/kafka.config.ts
@@ -1,14 +1,20 @@
 import Config from "./_base.config";
 
 type KafkaStringKeys =
-    | "KAFKA_BROKER_HOST"
+    | "KAFKA_BROKER_NODE"
 
 const kafkaKeys: KafkaStringKeys[] = [
-    "KAFKA_BROKER_HOST"
+    "KAFKA_BROKER_NODE"
 ]
 
 export default class KafkaConfig extends Config<KafkaStringKeys, never, never> {
     constructor(kafkaCfgName: string) {
         super(kafkaCfgName, kafkaKeys)
+    }
+
+    public getAllVars() {
+        return {
+            KAFKA_BROKER_NODE: this.GET_CONFIG_STRING("KAFKA_BROKER_NODE")
+        }
     }
 }
